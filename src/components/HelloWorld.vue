704f1fd8284b3b2e8872b089161b0e68f27c5f8b
everan-1994 <everan@aliyun.com>
2019-03-06 14:14:56 +0800

    change 1048 to 1024

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -70,7 +70,7 @@
         <div class="ui labeled button" tabindex="0">
             <div class="ui red button"><i class="heart icon"></i> Like </div>
             <a class="ui basic red left pointing label">
-                1,048
+                1,024
             </a>
         </div>
         <div class="ui labeled button" tabindex="0">
