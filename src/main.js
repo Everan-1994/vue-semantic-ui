57a1e4a6b0a73b3ff69639b62dd290e875de4bf0
everan-1994 <everan@aliyun.com>
2019-06-22 10:09:04 +0800

    add gogo

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
 
 import $ from 'jquery'
 
 import '../semantic/dist/semantic'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
